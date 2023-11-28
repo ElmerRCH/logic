2b5cb13056e20eb551eb3623489cfedb1a775f12
Elmer Camacho <renehiguera15@gmail.com>
2023-11-28 10:02:37 -0700

    comit

--- 03.py
+++ 03.py
@@ -1,13 +1,4 @@
 
 """
-    https://www.codewars.com/kata/62b3e38df90eac002c7a983f/train/python
+    https://www.codewars.com/kata/56dbed3a13c2f61ae3000bcd/train/python
 """
-def findSquares(m, n):
-    total_squares = 0
-
-    for i in range(1, n + 1):
-        total_squares += (m - i + 1) * (n - i + 1)
-    
-    return total_squares
-
-rectangulo_1 = findSquares(3, 2)
