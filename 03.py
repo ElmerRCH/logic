ae5f3179e460650dab36b9ffcfbbe7888b3ba8a8
elmerRCH <renehiguera15@gmail.com>
2023-11-28 19:58:06 -0700

    ejercicio-3

--- 03.py
+++ 03.py
@@ -1,9 +1,11 @@
 
 """
    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+   Create a function that takes an integer as an argument and
+   returns "Even" for even numbers or "Odd" for odd numbers.
 """
 def even_or_odd(number):
-    if number / 2 == 2:return 'Even' 
+    if number % 2 == 0:return 'Even' 
     return 'Odd'
 
 print(even_or_odd(7))
