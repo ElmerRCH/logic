df60010bb8f62af055f1be01aa3b4b9ce4772bcc
Elmer Camacho <renehiguera15@gmail.com>
2023-12-01 08:18:28 -0700

    logic

--- 07.py
+++ 07.py
@@ -1,3 +1,13 @@
 """
     https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/python
 """
+
+def is_digit(s):
+    if type(s) == str:
+        return s.isdigit()
+    elif type(s) == int:
+        return True
+    else:
+        return False
+
+print(is_digit('b'))
